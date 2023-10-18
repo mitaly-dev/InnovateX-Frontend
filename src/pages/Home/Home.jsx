6f41714cce8a3b4487e0d900d8f707bc23916693
mitaly-dev <mitalyy.dev@gmail.com>
2023-10-19 01:29:58 +0600

    product page created

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Advertised from "../Advertised/Advertised";
-import Categories from "../Categories/Categories";
 import About from "../About/About";
 import { useTitle } from "../../Hook/useTitle";
 import { useEffect } from "react";
 import Banner from "../../Components/Banner";
 import Gallery from "../../Components/Gallery";
 import Activities from "../../Components/Activities";
+import Categories from "../Categories/Categories";
 
 const Home = () => {
   useTitle("InnovateX");
